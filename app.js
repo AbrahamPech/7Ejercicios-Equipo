a86cf5b989c069f6dd53b26f684bc0626f064bd0
AbrahamPech <23090652@alumno.utmetropolitana.edu.mx>
2024-09-28 12:35:17 -0600

    ejercicios de matriz equipo | 1.0 | Creacion de app.js para manejo de la pagina, creacion de menu para linkear los ejercicios, creacion de los ejercicios 4 y 5

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// es un get para la pagina principa(menu.html)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'menu.html'));
+});
+
+// iniciador del servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
